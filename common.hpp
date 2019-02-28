20cf7d81d89c2a0210a87fd097494e8ebdb8c38f
MaciejNadolski98 <maciejnadolski98@gmail.com>
2019-02-28 18:57:12 +0100

    Slideshow

--- common.hpp
+++ common.hpp
@@ -1,3 +1,25 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+
+class Picture {
+public:
+  vector<string> tags;
+};
+
+vector<Picture> input;
+
+class Slide {
+public:
+  int first;
+  int second;
+};
+
+class Slideshow {
+public:
+  vector<Slide> show;
+
+  unsigned long long evaluate() {
+    
+  }
+}
