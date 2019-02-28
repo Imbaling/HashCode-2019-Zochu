f0594829ea65b1af7bd86359c2a56ae4b81fe79c
Wojciech Bączkowski <wojciech.baczkowski@students.mimuw.edu.pl>
2019-02-28 19:40:34 +0100

    startowe smieci

---
+++ wojt/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using vector<long long>
+
+
+int main() {
+
+    int n; // ile slajdow w pliku
+    scanf(" %d", &n);
+
+    return 0;
+}
